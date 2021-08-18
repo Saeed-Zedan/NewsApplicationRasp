a8504e3c3e555ee8cd8ed2d503787b19ab9bdf4d
Saeed Zidan <Saeed.z@sedco-online.com>
2021-08-18 18:28:42 +0300

    I think I missed up

---
+++ WindowsApp1/WindowsApp1/addNews.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class addNews
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.titleTextBox = New System.Windows.Forms.TextBox()
+        Me.descriptionTextBox = New System.Windows.Forms.TextBox()
+        Me.bodyTextBox = New System.Windows.Forms.TextBox()
+        Me.categoryComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.titleLabel = New System.Windows.Forms.Label()
+        Me.descriptionLabel = New System.Windows.Forms.Label()
+        Me.categoryLabel = New System.Windows.Forms.Label()
+        Me.bodyLabel = New System.Windows.Forms.Label()
+        Me.cancelButton = New System.Windows.Forms.Button()
+        Me.saveButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'titleTextBox
+        '
+        Me.titleTextBox.Location = New System.Drawing.Point(113, 12)
+        Me.titleTextBox.Name = "titleTextBox"
+        Me.titleTextBox.Size = New System.Drawing.Size(325, 20)
+        Me.titleTextBox.TabIndex = 0
+        '
+        'descriptionTextBox
+        '
+        Me.descriptionTextBox.Location = New System.Drawing.Point(113, 50)
+        Me.descriptionTextBox.Name = "descriptionTextBox"
+        Me.descriptionTextBox.Size = New System.Drawing.Size(325, 20)
+        Me.descriptionTextBox.TabIndex = 1
+        '
+        'bodyTextBox
+        '
+        Me.bodyTextBox.Location = New System.Drawing.Point(113, 132)
+        Me.bodyTextBox.Multiline = True
+        Me.bodyTextBox.Name = "bodyTextBox"
+        Me.bodyTextBox.Size = New System.Drawing.Size(325, 248)
+        Me.bodyTextBox.TabIndex = 3
+        '
+        'categoryComboBox1
+        '
+        Me.categoryComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.categoryComboBox1.FormattingEnabled = True
+        Me.categoryComboBox1.Items.AddRange(New Object() {"Politics", "Science", "Sport"})
+        Me.categoryComboBox1.Location = New System.Drawing.Point(113, 91)
+        Me.categoryComboBox1.Name = "categoryComboBox1"
+        Me.categoryComboBox1.Size = New System.Drawing.Size(325, 21)
+        Me.categoryComboBox1.Sorted = True
+        Me.categoryComboBox1.TabIndex = 2
+        '
+        'titleLabel
+        '
+        Me.titleLabel.AutoSize = True
+        Me.titleLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.titleLabel.Location = New System.Drawing.Point(7, 12)
+        Me.titleLabel.Name = "titleLabel"
+        Me.titleLabel.Size = New System.Drawing.Size(43, 20)
+        Me.titleLabel.TabIndex = 2
+        Me.titleLabel.Text = "Title"
+        '
+        'descriptionLabel
+        '
+        Me.descriptionLabel.AutoSize = True
+        Me.descriptionLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.descriptionLabel.Location = New System.Drawing.Point(7, 50)
+        Me.descriptionLabel.Name = "descriptionLabel"
+        Me.descriptionLabel.Size = New System.Drawing.Size(100, 20)
+        Me.descriptionLabel.TabIndex = 2
+        Me.descriptionLabel.Text = "Description"
+        '
+        'categoryLabel
+        '
+        Me.categoryLabel.AutoSize = True
+        Me.categoryLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.categoryLabel.Location = New System.Drawing.Point(7, 92)
+        Me.categoryLabel.Name = "categoryLabel"
+        Me.categoryLabel.Size = New System.Drawing.Size(81, 20)
+        Me.categoryLabel.TabIndex = 2
+        Me.categoryLabel.Text = "Category"
+        '
+        'bodyLabel
+        '
+        Me.bodyLabel.AutoSize = True
+        Me.bodyLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bodyLabel.Location = New System.Drawing.Point(7, 130)
+        Me.bodyLabel.Name = "bodyLabel"
+        Me.bodyLabel.Size = New System.Drawing.Size(49, 20)
+        Me.bodyLabel.TabIndex = 2
+        Me.bodyLabel.Text = "Body"
+        '
+        'cancelButton
+        '
+        Me.cancelButton.Location = New System.Drawing.Point(363, 386)
+        Me.cancelButton.Name = "cancelButton"
+        Me.cancelButton.Size = New System.Drawing.Size(75, 23)
+        Me.cancelButton.TabIndex = 3
+        Me.cancelButton.Text = "Cancel"
+        Me.cancelButton.UseVisualStyleBackColor = True
+        '
+        'saveButton
+        '
+        Me.saveButton.Location = New System.Drawing.Point(282, 386)
+        Me.saveButton.Name = "saveButton"
+        Me.saveButton.Size = New System.Drawing.Size(75, 23)
+        Me.saveButton.TabIndex = 4
+        Me.saveButton.Text = "Save"
+        Me.saveButton.UseVisualStyleBackColor = True
+        '
+        'addNews
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(450, 419)
+        Me.Controls.Add(Me.saveButton)
+        Me.Controls.Add(Me.cancelButton)
+        Me.Controls.Add(Me.bodyLabel)
+        Me.Controls.Add(Me.categoryLabel)
+        Me.Controls.Add(Me.descriptionLabel)
+        Me.Controls.Add(Me.titleLabel)
+        Me.Controls.Add(Me.categoryComboBox1)
+        Me.Controls.Add(Me.bodyTextBox)
+        Me.Controls.Add(Me.descriptionTextBox)
+        Me.Controls.Add(Me.titleTextBox)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "addNews"
+        Me.Text = "New news"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents titleTextBox As TextBox
+    Friend WithEvents descriptionTextBox As TextBox
+    Friend WithEvents bodyTextBox As TextBox
+    Friend WithEvents categoryComboBox1 As ComboBox
+    Friend WithEvents titleLabel As Label
+    Friend WithEvents descriptionLabel As Label
+    Friend WithEvents categoryLabel As Label
+    Friend WithEvents bodyLabel As Label
+    Friend WithEvents cancelButton As Button
+    Friend WithEvents saveButton As Button
+End Class
---
+++ WindowsApp1/WindowsApp1/addNews.vb
@@ -0,0 +1,44 @@
+﻿Imports System.IO
+Public Class addNews
+    Private Sub saveButton_Click(sender As Object, e As EventArgs) Handles saveButton.Click
+        Select Case String.Empty
+            Case titleTextBox.Text
+                MessageBox.Show("You must enter a title!", "Empty Title", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+                Exit Sub
+            Case categoryComboBox1.Text
+                MessageBox.Show("You must enter a category!", "Empty Title", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+                Exit Sub
+        End Select
+        If titleTextBox.Text = Nothing Then
+            MessageBox.Show("You must enter a title!", "Empty Title", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+        Else
+            Dim newsOb As News = New News()
+            newsOb.Title = titleTextBox.Text
+            newsOb.Body = bodyTextBox.Text
+            newsOb.Category = categoryComboBox1.Text
+            newsOb.Description = descriptionTextBox.Text
+
+            Try
+                Dim dirPath = My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\News"
+                If Not (Directory.Exists(dirPath)) Then
+                    FileSystem.MkDir(dirPath)
+                End If
+                Dim fileName = dirPath & "\" & Guid.NewGuid.ToString() & ".txt"
+                Dim fileWrite As FileStream = New FileStream(fileName, FileMode.Create, FileAccess.Write)
+                Dim streamWr As StreamWriter = New StreamWriter(fileWrite)
+
+                streamWr.Write(newsOb.Title & "^_^" & newsOb.creationDate & "^_^" & newsOb.Description & "^_^" & newsOb.Category & "^_^" & newsOb.Body)
+                streamWr.Flush()
+                fileWrite.Close()
+            Catch ex As IOException
+                MessageBox.Show("Process failed", "IO ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+            MessageBox.Show("Process end successfully", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Me.Dispose()
+        End If
+    End Sub
+
+    Private Sub cancelButton_Click(sender As Object, e As EventArgs) Handles cancelButton.Click
+        Me.Dispose()
+    End Sub
+End Class
